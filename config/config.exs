cc26dee6a76f4b6296d5275c89c2b8bc73feebe8
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-07-09 09:32:59 +0200

    🔥 Remove all plugins from default config

--- config/config.exs
+++ config/config.exs
@@ -67,9 +67,7 @@ config :kanta_test, Kanta,
   endpoint: KantaTestWeb.Endpoint,
   repo: KantaTest.Repo,
   otp_name: :kanta_test,
-  plugins: [
-    {Kanta.DeepL.Plugin, api_key: System.get_env("DEEPL_API_KEY")}
-  ]
+  plugins: []
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
