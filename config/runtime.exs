816213ba1759c49b061373db91eed2bae245df8d
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2025-03-13 12:12:13 +0100

    mix format

--- config/runtime.exs
+++ config/runtime.exs
@@ -69,8 +69,12 @@ if config_env() in [:prod, :dev] do
     endpoint: KantaTestWeb.Endpoint,
     repo: KantaTest.Repo,
     otp_name: :kanta_test,
-    plugins: 
-    [
-      # plugins that don't require additonal config and can be enabled by default go here
-    ] ++ if key = System.get_env("DEEPL_API_KEY"), do: {Kanta.DeepL.Plugin, api_key: key}, else: []
+    plugins:
+      [
+        # plugins that don't require additonal config and can be enabled by default go here
+      ] ++
+        if(key = System.get_env("DEEPL_API_KEY"),
+          do: {Kanta.DeepL.Plugin, api_key: key},
+          else: []
+        )
 end
